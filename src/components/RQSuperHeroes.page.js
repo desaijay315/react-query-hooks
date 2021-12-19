cdcb9c874ee9d29046fc085267e4cc7355cb39da
desaijay26 <desai26jay@gmail.com>
2021-12-19 15:00:16 +0530

    first commit

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,33 @@
+import { useQuery } from 'react-query';
+
+import axios from 'axios';
+
+const fetchSuperHeroes = () =>{
+  return axios.get('http://localhost:4000/superheroes')
+}
+
+export const RQSuperHeroesPage = () => {
+  const {isLoading, data, isError, error, isFetching} = useQuery('super-heroes', fetchSuperHeroes, {
+    staleTime: 1000,
+  })
+
+  if(isLoading){
+    return <h2>Loading...</h2>
+  }
+
+  console.log({isLoading, isFetching})
+
+
+  if(isError){
+    return <h2>{error.message}</h2>
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map(hero => {
+        return <div key={hero.name}>{hero.name}</div>
+      })}
+    </>
+  )
+}
